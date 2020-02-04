d5f9fc36f5ace86d36949136e80cbe1ce5fc5887
Xun <galaxymacos@gmail.com>
2020-02-04 10:54:30 -0500

    Initial commit

---
+++ Assets/CharacterMovement2D.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterMovement2D : MonoBehaviour
+{
+    public PlayerInput playerInput;
+    
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+    private Rigidbody2D rb;
+    
+    private bool facingRight = true;
+    
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatisGround;
+    
+    public int extraJumpsValue;
+    private int extraJumps;
+    private bool isJumping;
+    private float jumpTimeCounter;
+    public float jumpTime = 0.5f;
+    public UnityEvent CharacterJumpEvent;
+    public UnityEvent CharacterIdleEvent;
+    public UnityEvent CharacterRunEvent;
+    public UnityEvent CharacterDoubleJumpEvent;
+    
+    
+    private void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatisGround);
+    
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    
+        if (!facingRight && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+    
+    private void Update()
+    {
+        Jump();
+        
+        if (isGrounded && Math.Abs(moveInput) < Mathf.Abs(Mathf.Epsilon) && !isJumping)
+        {
+            CharacterIdleEvent?.Invoke();
+        }
+        if (isGrounded && Math.Abs(moveInput) > Mathf.Epsilon && !isJumping)
+        {
+            CharacterRunEvent?.Invoke();
+        }
+    
+    }
+    
+    /// <summary>
+    /// Dealing with jump behavior
+    /// </summary>
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            extraJumps = extraJumpsValue;
+        }
+    
+        // jump 
+        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0 && !isGrounded)
+        {
+            isJumping = true;
+            // rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+            jumpTimeCounter = jumpTime;
+            print("double jump");
+    
+            CharacterDoubleJumpEvent?.Invoke();
+            
+    
+        }
+        // jump 
+        else if (playerInput.jump && isGrounded)
+        {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            print("jump");
+            CharacterJumpEvent?.Invoke();
+    
+        }
+    
+        // jump 
+        if (Input.GetKey(KeyCode.UpArrow) && isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+    
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            isJumping = false;
+        }
+    
+        
+    }
+    
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    private InputActions inputAction;
+
+    public bool jump;
+
+    public float horizontalMovement;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        inputAction = new InputActions();
+        inputAction.Player.Jump.performed += ctx => jump = true;
+    }
+
+    private void OnEnable()
+    {
+        inputAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputAction.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
