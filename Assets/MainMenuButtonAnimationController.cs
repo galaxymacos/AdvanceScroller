374149727ec2b031c97e610c07e16eb5f25b34ef
Xun <galaxymacos@gmail.com>
2020-02-14 00:27:19 -0500

    Test Unity UIBuilder

---
+++ Assets/MainMenuButtonAnimationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class MainMenuButtonAnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void ButtonPressed()
+    {
+        animator.SetTrigger("pressed");
+    }
+}
---
+++ Assets/OptionMenuAnimatorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class OptionMenuAnimatorController : MonoBehaviour
+{
+    private Animator animator;
+    
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void EnableOptionMenu()
+    {
+        animator.SetTrigger("enable");
+    }
+}
