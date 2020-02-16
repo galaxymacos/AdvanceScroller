ddfc5bbfea0c23dcf8aca66ce51e36c55c13de3c
Xun <galaxymacos@gmail.com>
2020-02-15 22:37:30 -0500

    Update UI Messager

---
+++ Assets/UIMessager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMessager : MonoBehaviour
+{
+    public List<PlayerPanel> playerPanels;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        PlayerCharacterSpawner.instance.onPlayerSpawnFinished += Setup;
+    }
+
+
+    public void Setup()
+    {
+        List<PlayerCharacter> characters = PlayerCharacterSpawner.instance.charactersForPlayer;
+        
+        
+        for (int i = 0; i < playerPanels.Count; i++)
+        {
+            
+        }
+    }
+}
