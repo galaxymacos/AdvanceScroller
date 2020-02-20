228489527920250483865586f9eeb28b9b595461
Xun <galaxymacos@gmail.com>
2020-02-19 22:24:15 -0500

    Add a skill time counter panel

---
+++ Assets/PlayerSkillPanel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class PlayerSkillPanel : MonoBehaviour
+{
+    public PlayerCharacter owner;
+    public List<Image> skillCooldownIndicators;
+    public List<Skill> skills;
+    private bool hasSetup;
+
+    public Action onPlayerSetup;
+    
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        onPlayerSetup += Setup;
+    }
+
+    public void Setup()
+    {
+        hasSetup = true;
+        SkillCooldownManager skillManager = owner.GetComponent<SkillCooldownManager>();
+        
+        foreach (var skill in skillManager.skills)
+        {
+            switch (skill.skillName)
+            {
+                case "skill1":
+                    
+                    skills.Add(skill);
+                    break;
+                
+            }
+        }
+        
+        foreach (var skill in skillManager.skills)
+        {
+            switch (skill.skillName)
+            {
+                case "skill2":
+                    
+                    skills.Add(skill);
+                    break;
+                
+            }
+        }
+        
+        foreach (var skill in skillManager.skills)
+        {
+            switch (skill.skillName)
+            {
+                case "skill3":
+                    
+                    skills.Add(skill);
+                    break;
+                
+            }
+        }
+        
+        foreach (var skill in skillManager.skills)
+        {
+            switch (skill.skillName)
+            {
+                case "skill4":
+                    
+                    skills.Add(skill);
+                    break;
+                
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasSetup) return;
+
+        for (int i = 0; i < skillCooldownIndicators.Count; i++)
+        {
+            skillCooldownIndicators[i].fillAmount = skills[i].coolDownCounter / skills[i].coolDown;
+        }
+    }
+}
--- Assets/Scripts/UIMessager.cs
+++ Assets/Scripts/UIMessager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class UIMessager : MonoBehaviour
 {
     public List<PlayerPanel> playerPanels;
+    public List<PlayerSkillPanel> playerSkillPanels;
     // Start is called before the first frame update
     
     private void Awake()
@@ -19,7 +20,6 @@ public class UIMessager : MonoBehaviour
         List<PlayerCharacter> characters = PlayerCharacterSpawner.instance.charactersForPlayer;
         if (characters.Count == playerPanels.Count)
         {
-            print(characters.Count);
             for (int i = 0; i < characters.Count; i++)
             {
                 playerPanels[i].player = characters[i];
@@ -29,6 +29,20 @@ public class UIMessager : MonoBehaviour
         foreach (PlayerPanel playerPanel in playerPanels)
         {
             playerPanel.onPlayerSetup?.Invoke();
+        }
+        
+        
+
+        if (characters.Count == playerSkillPanels.Count)
+        {
+            for (int i = 0; i < characters.Count; i++)
+            {
+                playerSkillPanels[i].owner = characters[i];
+            }
+        }
+        foreach (PlayerSkillPanel playerSkillPanel in playerSkillPanels)
+        {
+            playerSkillPanel.onPlayerSetup?.Invoke();
         }
     }
 }
