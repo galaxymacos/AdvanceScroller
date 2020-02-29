e3c7fb9f92f58f43401c255da91d4cf6734d0ef2
apollokevin <apollokevin2012@gmail.com>
2020-02-29 16:19:17 -0500

    Rain Particle System and Particle System Pauser

---
+++ Assets/ParticleSystemPauser.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemPauser : MonoBehaviour ,IPauseable
+{
+    private bool isPausing;
+    [SerializeField]
+    private ParticleSystem rainGenerator;
+    
+
+    
+
+
+    public void Pause()
+    {
+        if (isPausing)
+        {
+            print("rain wants to stop");
+            return;
+        }
+        rainGenerator.Pause();
+
+        isPausing = true;
+    }
+
+    public void UnPause()
+    {
+        if (!isPausing)
+        {
+            print("rain wants to rain");
+            return;
+        }
+        rainGenerator.Play();
+
+        isPausing = false;
+    }
+
+   
+    
+}
