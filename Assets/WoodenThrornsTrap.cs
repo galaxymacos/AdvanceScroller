9b4f1fec94b8c49e239ced70e69bf599d95d79b1
apollokevin <apollokevin2012@gmail.com>
2020-02-29 20:52:50 -0500

    add collider to traps and add one platform

---
+++ Assets/WoodenThrornsTrap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodenThrornsTrap : MonoBehaviour
+{
+    public DamageData damageData;
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var damageReceiver = other.GetComponent<DamageReceiver>();
+        if (damageReceiver != null)
+        {
+            damageReceiver.Analyze(damageData, transform);
+            
+        }
+    }
+   
+    
+
+}
