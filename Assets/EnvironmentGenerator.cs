d0f6c2cf81edc03112ece0717a83d0e7be84f39a
apollokevin <apollokevin2012@gmail.com>
2020-03-02 21:00:32 -0500

    Add wind zone

---
+++ Assets/EnvironmentGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentGenerator : MonoBehaviour
+{
+
+    [SerializeField]
+    private ParticleSystem TargetParticleSystem;
+    private float timer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        particleSystemValueControl();
+    }
+    private void particleSystemValueControl()
+    {
+        
+    }
+}
