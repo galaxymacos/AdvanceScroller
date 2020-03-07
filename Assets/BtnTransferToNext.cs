c7070043b340a5eb81eb821a5cb59d0f57645b39
Xun <galaxymacos@gmail.com>
2020-03-07 03:44:42 -0500

    Upgrade unity to 3.4 and ready to add save/load system

---
+++ Assets/BtnTransferToNext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This button will transfer to the next panel 
+/// </summary>
+public class BtnTransferToNext : MonoBehaviour
+{
+    public GameObject currentPanel;
+    public GameObject nextPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerCharacterSpawner.cs
+++ Assets/Scripts/PlayerCharacterSpawner.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class PlayerCharacterSpawner : MonoBehaviour
 {
-
     // public List<PlayerInput> playerInputs;
     public List<GameObject> selectedHeros;
 
@@ -41,10 +42,11 @@ public class PlayerCharacterSpawner : MonoBehaviour
         if (mapPlayerSpawnData == null)
         {
             Debug.LogError("MapPlayerSpawnData not found");
-            Debug.LogError("You need to tell the player this class where to generate the players in the map by create a MapPlayerSpawnData class under your map");
-            
+            Debug.LogError(
+                "You need to tell the player this class where to generate the players in the map by create a MapPlayerSpawnData class under your map");
         }
     }
+
 
     // Start is called before the first frame update
     void Start()
@@ -52,54 +54,113 @@ public class PlayerCharacterSpawner : MonoBehaviour
         charactersForPlayer = new List<PlayerCharacter>();
         for (int i = 0; i < selectedHeros.Count; i++)
         {
-            GameObject hero = Instantiate(selectedHeros[i], new Vector2(0+i*4, 0), Quaternion.identity);
-            hero.transform.position = mapPlayerSpawnData.PlayerSpawnPositions[i].position;
-            hero.layer = layermask_to_layer(whatIsPlayer[i]);
-            hero.SetActive(true);
-            charactersForPlayer.Add(hero.GetComponent<PlayerCharacter>());
+            // GameObject hero = Instantiate(selectedHeros[i]);
+            // hero.transform.position = mapPlayerSpawnData.PlayerSpawnPositions[i].position;
+            // hero.layer = layermask_to_layer(whatIsPlayer[i]);
+            // charactersForPlayer.Add(hero.GetComponent<PlayerCharacter>());
+            
+            AddPlayer(selectedHeros[i]);
         }
 
 
-        for (int i = 0; i < charactersForPlayer.Count; i++)
-        {
-            for (int j = 0; j < charactersForPlayer.Count; j++)
-            {
-                if (i != j)
-                {
-                    charactersForPlayer[i].whatIsEnemy |= 1<<charactersForPlayer[j].gameObject.layer;
-                    // charactersForPlayer[i].whatIsEnemy &= ~(1 << charactersForPlayer[j].gameObject.layer);
-
-                    charactersForPlayer[i].whatIsGround |= 1 << charactersForPlayer[j].gameObject.layer;
-                    // charactersForPlayer[i].whatIsGround &= ~(1 << charactersForPlayer[j].gameObject.layer);
-
-                }
-            }
-                
-        }
-        
-        for (int i = 0; i < charactersForPlayer.Count; i++)
-        {
-            for (int j = 0; j < charactersForPlayer.Count; j++)
-            {
-                if (i != j)
-                {
-                }
-            }
-                
-        }
+        // for (int i = 0; i < charactersForPlayer.Count; i++)
+        // {
+        //     for (int j = 0; j < charactersForPlayer.Count; j++)
+        //     {
+        //         if (i != j)
+        //         {
+        //             charactersForPlayer[i].whatIsEnemy |= 1 << charactersForPlayer[j].gameObject.layer;
+        //             // charactersForPlayer[i].whatIsEnemy &= ~(1 << charactersForPlayer[j].gameObject.layer);
+        //
+        //             charactersForPlayer[i].whatIsGround |= 1 << charactersForPlayer[j].gameObject.layer;
+        //             // charactersForPlayer[i].whatIsGround &= ~(1 << charactersForPlayer[j].gameObject.layer);
+        //         }
+        //     }
+        // }
         
         
         onPlayerSpawnFinished?.Invoke();
-        
     }
-    
-    public static int layermask_to_layer(LayerMask layerMask) {
+
+    public void AddPlayer(GameObject heroToSpawn)
+    {
+        GameObject hero = Instantiate(heroToSpawn);
+
+        hero.transform.position = mapPlayerSpawnData.PlayerSpawnPositions[charactersForPlayer.Count].position;
+        hero.layer = layermask_to_layer(whatIsPlayer[charactersForPlayer.Count]);
+        charactersForPlayer.Add(hero.GetComponent<PlayerCharacter>());
+
+        for (int i = 0; i < charactersForPlayer.Count; i++)
+        {
+            for (int j = 0; j < charactersForPlayer.Count; j++)
+            {
+                if (i != j)
+                {
+                    charactersForPlayer[i].whatIsEnemy |= 1 << charactersForPlayer[j].gameObject.layer;
+
+                    charactersForPlayer[i].whatIsGround |= 1 << charactersForPlayer[j].gameObject.layer;
+                }
+            }
+        }
+
+    }
+
+
+    public static int layermask_to_layer(LayerMask layerMask)
+    {
         int layerNumber = 0;
         int layer = layerMask.value;
-        while(layer > 0) {
+        while (layer > 0)
+        {
             layer = layer >> 1;
             layerNumber++;
         }
+
         return layerNumber - 1;
+    }
+}
+
+public class ChampionSelectionData
+{
+    public List<GameObject> champions;
+    public int fightSceneIndex;
+
+    public ChampionSelectionData(List<GameObject> champions)
+    {
+        this.champions = champions;
+    }
+}
+
+public static class SaveSystem
+{
+    public static void SaveHeroSelectionData(ChampionSelectionData championSelectionData)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/player.fun";
+        FileStream stream = new FileStream(path, FileMode.Create);
+        
+        formatter.Serialize(stream,championSelectionData);
+        stream.Close();
+    }
+
+    public static ChampionSelectionData LoadHeroSelectionData()
+    {
+        string path = Application.persistentDataPath + "/player.fun";
+
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            ChampionSelectionData championSelectionData = formatter.Deserialize(stream) as ChampionSelectionData;
+            stream.Close();
+            
+            return championSelectionData;
+        }
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
     }
 }
