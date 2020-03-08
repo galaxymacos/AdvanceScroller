850fb24462ec732715b7ce00b507826fb598b8c1
Xun <galaxymacos@gmail.com>
2020-03-08 01:13:53 -0500

    Add champion selected effect

--- Assets/SelectionPanelElement.cs
+++ Assets/SelectionPanelElement.cs
@@ -9,23 +9,11 @@ public class SelectionPanelElement : MonoBehaviour
     public SelectionPanelElement topElement;
     public SelectionPanelElement downElement;
 
+    public Action onSelected;
+    public Action onDeselected;
+
     [Tooltip("What champion does this sprite indicate")]
     public GameObject champion;
-
-    
 }
 
-public class ChampionSelectionCommand : Command
-{
-    public GameObject championToSelect;
 
-    public ChampionSelectionCommand(GameObject championToSelect)
-    {
-        this.championToSelect = championToSelect;
-    }
-    
-    public void Execute()
-    {
-        // Add the champion selected to the list of champions and disable the current pointer because 
-    }
-}
