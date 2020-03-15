de736c6a4f48c7e666f42c8c93b8ef0f44866641
apollokevin <apollokevin2012@gmail.com>
2020-03-15 07:45:20 -0400

    Add sign rotation anim to Nature map sign

---
+++ Assets/SignRoationAnimControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class SignRoationAnimControl : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] RotatedSigns;
+    Animator animator;
+    bool signIsTouched;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerCharacter playerCharacter = other.GetComponent<PlayerCharacter>();
+        if (playerCharacter != null)
+        {
+            print("ENTER if");
+            animator.Play("SignsRotation");
+            signIsTouched = animator.GetBool("playerComes");
+            animator.SetBool("SignsRotation", !signIsTouched);
+            
+        }
+       
+    }
+}
