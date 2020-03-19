e777efb8747b2537fd389c2269c926c28096f22f
Xun <galaxymacos@gmail.com>
2020-03-19 17:15:35 -0400

    Update audio manager

--- Assets/AudioObject.cs
+++ Assets/AudioObject.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 [Serializable]
 public class AudioObject
 {
-    public AudioType type;
+    public string typeInString;
+    public AudioType type
+    {
+        get
+        {
+            bool parseResult = Enum.TryParse(typeInString, true, out AudioType result);
+            if (!parseResult)
+            {
+                Debug.LogError($"Can't find the audio type with the name {typeInString}");
+            }
+            return result;
+        }
+    }
+
     public AudioClip clip;
 }
