5656098d166a148e8801f47d71601803cc4b6ca3
Xun <galaxymacos@gmail.com>
2020-04-04 19:11:52 -0400

    Ready Countdown Added

---
+++ Assets/FreezePlayerOnReady.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezePlayerOnReady : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameStateMachine.gameIsPause = true;
+    }
+
+    private void OnDisable()
+    {
+        GameStateMachine.gameIsPause = false;
+    }
+}
