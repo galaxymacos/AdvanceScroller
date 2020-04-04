56c4a8fde11f7c8c7c5159269e3688e2d517bbf7
Xun <galaxymacos@gmail.com>
2020-04-04 18:28:05 -0400

    Add a loading bar

---
+++ Assets/ProgressBarUpdateComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarUpdateComponent : MonoBehaviour
+{
+    [SerializeField] private LoadSceneOnTrigger loadSceneOnTrigger;
+    private Slider slider;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+        loadSceneOnTrigger.onLoadingPercentageUpdated += UpdateValue;
+    }
+
+    private void OnDestroy()
+    {
+        loadSceneOnTrigger.onLoadingPercentageUpdated -= UpdateValue;
+    }
+
+    private void UpdateValue(float obj)
+    {
+        slider.value = Mathf.Clamp01(obj);
+    }
+
+
+}
--- Assets/Scripts/LoadSceneOnTrigger.cs
+++ Assets/Scripts/LoadSceneOnTrigger.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class LoadSceneOnTrigger : MonoBehaviour
@@ -9,20 +11,45 @@ public class LoadSceneOnTrigger : MonoBehaviour
 
     [SerializeField] private bool loadCurrentScene;
 
+    public UnityEvent onBeforeLoading;
+    public event Action<float> onLoadingPercentageUpdated;
     private bool hasTriggered;
     public void Trigger()
     {
 
         if (hasTriggered) return;
         hasTriggered = true;
+        onBeforeLoading?.Invoke();
         if (loadCurrentScene)
         {
-            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+            StartCoroutine(LoadingScene(SceneManager.GetActiveScene().name));
         }
         else
         {
-            SceneManager.LoadSceneAsync(sceneToLoad);
+            StartCoroutine(LoadingScene(sceneToLoad));
         }
+    }
+
+    public IEnumerator LoadingScene(string sceneNameToLoad)
+    {
+        var operation = SceneManager.LoadSceneAsync(sceneNameToLoad);
+        operation.allowSceneActivation =     false;
+        while (!operation.isDone)
+        {
+            var progress = Mathf.Clamp01(operation.progress / 0.9f);
+            onLoadingPercentageUpdated?.Invoke(progress);
+
+            if (progress >= 0.9f)
+            {
+                onLoadingPercentageUpdated?.Invoke(1);
+                yield return new WaitForSeconds(1.5f);
+                operation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+        // yield return new WaitForSeconds(1.5f);
+
     }
     
     public void Trigger(string _sceneToLoad)
